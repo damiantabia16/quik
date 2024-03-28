de4aa54c837fff9b96ecf3725566bbdfd1e08af2
damiantabia16 <damiantabia16@gmail.com>
2024-03-28 17:44:47 -0300

    Cambios en URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { API_URL } from '../config';
 
 const instance = axios.create({
-    baseURL: "http://localhost:4000/api",
+    baseURL: "http://localhost:4000/api" || "https://quik-server.vercel.app/",
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
