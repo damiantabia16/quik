985c94c4666af25b8dda237b351b0d1a2a24772d
damiantabia16 <damiantabia16@gmail.com>
2024-03-29 22:42:29 -0300

    Cambio en axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { API_URL } from '../config';
 
 const instance = axios.create({
-    baseURL: API_URL,
+    baseURL: "https://quik-server.vercel.app/api",
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
