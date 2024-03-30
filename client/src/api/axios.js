edccb2eb392d60a76a735b2e05b53a5760ce2fec
damiantabia16 <damiantabia16@gmail.com>
2024-03-29 23:33:07 -0300

    Otro cambio en axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const token = Cookies.get('token');
+const cookies = Cookies.get();
 
 const instance = axios.create({
     baseURL: "https://quik-server.vercel.app/api",
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + token
+        'Authorization': 'Bearer ' + cookies.token
     }
 });
 
