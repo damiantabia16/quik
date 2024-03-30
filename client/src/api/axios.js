a990a4d824c9d59da2de24dbcf32367c7461d4dc
damiantabia16 <damiantabia16@gmail.com>
2024-03-30 20:52:39 -0300

    Interceptores añadidos en axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,22 +1,33 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const token = Cookies.get('token');
-
 const instance = axios.create({
     baseURL: "https://quik-server.vercel.app/api",
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+instance.interceptors.request.use(config => {
+    const token = Cookies.get('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
 });
 
 instance.interceptors.response.use(
-    (response) => response,
-    async (error) => {
+    response => response,
+    async error => {
         const originalRequest = error.config;
         if (error.response.status === 401 && !originalRequest._retry) {
             originalRequest._retry = true;
             try {
-                const res = await axios.post('/auth/refresh-token');
-                instance.defaults.headers.common['Authorization'] = `Bearer ${res.data.accessToken}`;
+                const res = await instance.post('/auth/refresh-token');
+                const newToken = res.data.accessToken;
+                Cookies.set('token', newToken); // Actualiza el token en las cookies
+                originalRequest.headers.Authorization = `Bearer ${newToken}`;
                 return instance(originalRequest);
             } catch (refreshError) {
                 throw refreshError;
